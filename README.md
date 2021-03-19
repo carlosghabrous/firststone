ece401921ffc7dd913d9d5c8e85e1946442d76e9
carlosghabrous <carlos.ghabrous@gmail.com>
2021-03-19 20:04:15 +0100

    First commit

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 First Stone is a utility that automates the creation of project's directories and files for certain programming languages. 
 Current supported languages are: Python, Go. 
-
+    
 
 
--- firststone.go
+++ firststone.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const minimumArgumentsNumber int = 1
 
 func main() {
-	fmt.Println("First stone")
+	if len(os.Args[1:]) < minimumArgumentsNumber {
+		fmt.Println("Cannot do anything with that!")
+		fmt.Println("Print help/usage or whatever")
+	}
+
+	command := os.Args[1]
+	if command == "help" {
+		help_cmd()
+
+	} else if command == "init" {
+		init_cmd(os.Args[2:])
+
+	} else {
+		fmt.Println("unknown command")
+		fmt.Println("Print help/usage or whatever")
+		os.Exit(1)
+	}
+
+}
+
+func help_cmd() {
+	usage := "firststone <command> [flags] <project-name> <language>"
+	usage += "Available commands:"
+	usage += "init -> starts project"
+	usage += "help -> prints this or a command's help"
+	fmt.Println(usage)
+}
+
+func init_cmd(commandsAndFlags []string) error {
+	fmt.Println("Execute init command with arguments", commandsAndFlags)
+	projectName, language := commandsAndFlags[0], commandsAndFlags[1]
+	defaultPermissions := 0755
+
+	err := os.Mkdir(projectName, os.FileMode(defaultPermissions))
+	if err != nil {
+		fmt.Println("Error while creating directory ", projectName)
+		return err
+	}
+
+	defer os.RemoveAll(projectName)
+
+	if language == "python" {
+		fmt.Println("Making python project")
+
+	} else if language == "go" {
+		fmt.Println("Making go project")
+
+	} else {
+		fmt.Println("Language not supported")
+		os.Exit(1)
+	}
+
+	return nil
 }
