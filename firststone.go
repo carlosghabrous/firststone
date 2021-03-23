2839b8c37b9fb96fef0a5111b11e351ae7595185
carlosghabrous <carlos.ghabrous@gmail.com>
2021-03-23 20:50:48 +0100

    Simplify main function. Add command type and run method for completeness

--- firststone.go
+++ firststone.go
@@ -5,79 +5,102 @@ import (
 	"os"
 )
 
-const minimumArgumentsNumber int = 1
+const minimumArgumentsNumber int = 2
+
+type FirstStoneCommand struct {
+	cmdName string
+}
+
+func (fsCmd FirstStoneCommand) run(cmdFlagsSubs ...string) {
+	fmt.Printf("Running command %v\n", fsCmd.cmdName)
+}
+
+var commandsRegistry = make(map[string]FirstStoneCommand)
+
+func init() {
+	commandsRegistry["init"] = FirstStoneCommand{cmdName: "init"}
+	commandsRegistry["help"] = FirstStoneCommand{cmdName: "help"}
+}
 
 func main() {
-	if len(os.Args[1:]) < minimumArgumentsNumber {
-		fmt.Println("Cannot do anything without arguments!")
-		help_cmd()
+
+	helpCommand := commandsRegistry["help"]
+
+	if len(os.Args) < minimumArgumentsNumber {
+		fmt.Println("Missing command!")
+		helpCommand.run()
 		os.Exit(1)
 	}
 
-	command := os.Args[1]
-	if command == "help" {
-		help_cmd()
+	commandName := os.Args[1]
+	command, ok := commandsRegistry[commandName]
 
-	} else if command == "init" {
-		err := init_cmd(os.Args[2:])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-	} else {
-		fmt.Println("unknown command")
-		help_cmd()
+	if !ok {
+		fmt.Printf("Unknown command %v\n", commandName)
+		helpCommand.run()
 		os.Exit(1)
 	}
 
+	command.run(os.Args[2:]...)
 }
 
-func help_cmd() {
-	usage := "firststone <command> [flags] <project-name> <language>\n" +
-		"Available commands:\n" +
-		"init -> starts project\n" +
-		"help -> prints this or a command's help\n"
+// command := os.Args[1]
+// if command == "help" {
+// 	help_cmd()
 
-	fmt.Println(usage)
-}
+// } else if command == "init" {
+// 	err := init_cmd(os.Args[2:])
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
 
-func init_cmd(commandsAndFlags []string) error {
-	projectName, language := commandsAndFlags[0], commandsAndFlags[1]
+// } else {
+// 	fmt.Println("unknown command")
+// 	help_cmd()
+// 	os.Exit(1)
+// }
 
-	if language == "python" {
-		cwd, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Could not get current working directory!")
-			os.Exit(1)
-		}
+// func help_cmd() {
+// 	usage := "firststone <command> [flags] <project-name> <language>\n" +
+// 		"Available commands:\n" +
+// 		"init -> starts project\n" +
+// 		"help -> prints this or a command's help\n"
 
-		err = initProject(cwd, language, projectName)
-		if err != nil {
-			fmt.Println("Error while copying files")
-		}
+// 	fmt.Println(usage)
+// }
 
-	} else if language == "go" {
-		cwd, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Could not get current working directory!")
-			os.Exit(1)
-		}
+// func init_cmd(commandsAndFlags []string) error {
+// 	projectName, language := commandsAndFlags[0], commandsAndFlags[1]
 
-		err = initProject(cwd, language, projectName)
-		if err != nil {
-			fmt.Println("Error while copying files")
-		}
+// 	if language == "python" {
+// 		cwd, err := os.Getwd()
+// 		if err != nil {
+// 			fmt.Println("Could not get current working directory!")
+// 			os.Exit(1)
+// 		}
 
-	} else {
-		fmt.Println("Language not supported")
-		os.Exit(1)
-	}
+// 		err = initProject(cwd, language, projectName)
+// 		if err != nil {
+// 			fmt.Println("Error while copying files")
+// 		}
 
-	return nil
-}
+// 	} else if language == "go" {
+// 		cwd, err := os.Getwd()
+// 		if err != nil {
+// 			fmt.Println("Could not get current working directory!")
+// 			os.Exit(1)
+// 		}
 
-func initProject(dest, language, projectName string) error {
+// 		err = initProject(cwd, language, projectName)
+// 		if err != nil {
+// 			fmt.Println("Error while copying files")
+// 		}
 
-	return nil
-}
+// 	} else {
+// 		fmt.Println("Language not supported")
+// 		os.Exit(1)
+// 	}
+
+// 	return nil
+// }
