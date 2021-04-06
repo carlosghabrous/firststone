e0faf22f0dde47637f5cf2f2024ed32eb531c579
carlosghabrous <carlos.ghabrous@gmail.com>
2021-04-06 18:35:22 +0200

    Firststone-6: Add testing

--- skeletons/generic_test.go
+++ skeletons/generic_test.go
@@ -1,6 +1,8 @@
 package skeletons
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -17,4 +19,45 @@ func TestDirExistsRealDir(t *testing.T) {
 	if !dirExists(homeDir) {
 		t.Errorf("dirExists(%s) = false; want true", homeDir)
 	}
+}
+
+func TestIsCwdEmptyWithEmptyDir(t *testing.T) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error while getting current directory\n")
+	}
+
+	dir, err := ioutil.TempDir(currentDir, "tempDir")
+	if err != nil {
+		fmt.Printf("Error while creating temp dir %v tempDir\n", dir)
+	}
+	defer os.RemoveAll(dir)
+
+	if empty, _ := isCwdEmpty(dir); !empty {
+		t.Errorf("Directory %v should be empty, but it is not\n", dir)
+	}
+}
+
+func TestIsCwdEmptyWithNonEmptyDir(t *testing.T) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error while getting current directory\n")
+	}
+
+	dir, err := ioutil.TempDir(currentDir, "tempDir")
+	if err != nil {
+		fmt.Printf("Error while creating temp dir %v tempDir\n", dir)
+	}
+	defer os.RemoveAll(dir)
+
+	file, err := ioutil.TempFile(dir, "some_file")
+	if err != nil {
+		fmt.Printf("Error while creating file %v\n", file.Name())
+	}
+	defer os.Remove(file.Name())
+
+	if empty, _ := isCwdEmpty(dir); empty {
+		t.Errorf("Directory %v should NOT be empty, but it is\n", dir)
+	}
+
 }
