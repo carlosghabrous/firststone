1e5f907ae306e34438e2733232184d25b62cdbb8
carlosghabrous <carlos.ghabrous@gmail.com>
2021-04-13 10:08:25 +0200

    Firststone-6: Handle errors, refactor to allow testing

--- main.go
+++ main.go
@@ -21,68 +21,86 @@ func main() {
 		os.Exit(1)
 	}
 
-	command := os.Args[1]
-	if command == "init" {
-		initCmd(os.Args[2:]...)
-
-	} else if command == "help" {
-		helpCmd(os.Args[2:]...)
-
-	} else if command == "clean" {
-		cleanCmd(os.Args[2:]...)
-
-	} else {
-		fmt.Println("Unknown command")
+	if err := dispatchCommand(os.Args[1]); err != nil {
+		fmt.Printf("Error: %v\n", err)
 		usage()
 		os.Exit(1)
 	}
 }
 
-func initCmd(commands ...string) {
+func dispatchCommand(command string) (err error) {
+
+	if command == "init" {
+		err = initCmd(os.Args[2:]...)
+
+	} else if command == "help" {
+		err = helpCmd(os.Args[2:]...)
+
+	} else if command == "clean" {
+		err = cleanCmd(os.Args[2:]...)
+
+	} else {
+		err = fmt.Errorf("Unknown command")
+	}
+
+	return
+}
+
+func initCmd(commands ...string) (err error) {
 	initCommand := flag.NewFlagSet("init", flag.ExitOnError)
 	initHelp := initCommand.Bool("help", false, "help")
 
 	initCommand.Parse(commands)
-	projectLanguage := unpackArgs(initCommand.Args()...)
+	projectLanguage, err := unpackArgs(initCommand.Args()...)
+	if err != nil {
+		return fmt.Errorf("Wrong input commands %v\n", err)
+	}
 
 	if *initHelp {
 		usageInitCmd()
+		return
 	}
 
 	if err := skeletons.CreateProject(projectLanguage); err != nil {
-		fmt.Printf("Could not create project -> %v\n", err)
+		return fmt.Errorf("Could not create project -> %v\n", err)
 	}
+
+	return
 }
 
-func helpCmd(commands ...string) {
+func helpCmd(commands ...string) (err error) {
 	usage()
-
+	return
 }
 
-func cleanCmd(commands ...string) {
+func cleanCmd(commands ...string) (err error) {
 	cleanCommand := flag.NewFlagSet("clean", flag.ExitOnError)
 	cleanHelp := cleanCommand.Bool("help", false, "help")
 	cleanCommand.Parse(commands)
 
-	projectLanguage := unpackArgs(cleanCommand.Args()...)
+	projectLanguage, err := unpackArgs(cleanCommand.Args()...)
+	if err != nil {
+		return fmt.Errorf("Wrong input commands %v\n", err)
+	}
 
 	if *cleanHelp {
 		usageHelpCmd()
+		return
 	}
 
 	if err := skeletons.CleanProject(projectLanguage); err != nil {
-		fmt.Printf("Could not clean project -> %v\n", err)
+		return fmt.Errorf("Could not clean project -> %v\n", err)
 	}
 
+	return
 }
 
-func unpackArgs(args ...string) string {
+func unpackArgs(args ...string) (string, error) {
 	if len(args) < 1 {
-		fmt.Println("Two arguments expected, project language!")
-		os.Exit(1)
+		return "", fmt.Errorf("Two arguments expected, project language!")
 	}
 
-	return args[0]
+	return args[0], nil
 }
 
 func usageInitCmd() {
