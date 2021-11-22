01af1d993ff55e4ad2dcea003befde06b3616563
carlosghabrous <carlos.ghabrous@gmail.com>
2021-11-22 07:42:54 +0100

    Scan supported languages

--- lang/general.go
+++ lang/general.go
@@ -1,6 +1,49 @@
 package lang
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
+	"strings"
+)
+
+const languageDirName = "lang"
+
+var langDir = path.Join(".", languageDirName)
+var supportedLangs = make(map[string]bool)
+var filesToExclude map[string]bool
+
+func init() {
+	initFilesToExclude()
+
+	files, err := ioutil.ReadDir(langDir)
+	if err != nil {
+		log.Fatal("Could not scan languages directory!")
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fileName := file.Name()
+
+		if _, ok := filesToExclude[fileName]; ok {
+			continue
+		}
+
+		if idx := strings.Index(fileName, ".go"); idx != -1 {
+			supportedLangs[fileName[:idx]] = true
+		}
+	}
+}
+
+func initFilesToExclude() {
+	filesToExclude = map[string]bool{
+		"general.go": true,
+	}
+}
 
 func LanguageSupported(language string) error {
 	fmt.Println("Language is supported")
