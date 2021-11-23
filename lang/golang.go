1ada0d7bf3056a3cf030d27990960526f4057124
carlosghabrous <carlos.ghabrous@gmail.com>
2021-11-23 12:52:36 +0100

    Add printouts to check code correctness

--- lang/golang.go
+++ lang/golang.go
@@ -5,13 +5,11 @@ import "fmt"
 type GolangProject Project
 
 func (p GolangProject) CheckNamingConventions() error {
-	fmt.Println("Checking Golang naming conventions")
-
+	fmt.Printf("Checking naming conventions for project %s\n", p.Name)
 	return nil
 }
 
 func (p GolangProject) Build() error {
-	fmt.Println("Building Golang Project")
-
+	fmt.Printf("Building %s project %s\n", p.Language, p.Name)
 	return nil
 }
--- lang/python.go
+++ lang/python.go
@@ -4,12 +4,19 @@ import "fmt"
 
 type PythonProject Project
 
+var pythonProjectItems = []projectItem{
+	{name: "item1", parent: "parent1", permission: "permission1", content: "content1"},
+}
+
 func (p PythonProject) CheckNamingConventions() error {
-	fmt.Println("Checking Python naming conventions")
+	fmt.Printf("Checking naming conventions for project %s\n", p.Name)
 	return nil
 }
 
 func (p PythonProject) Build() error {
-	fmt.Println("Building Python Project")
+	fmt.Printf("Building %s project %s\n", p.Language, p.Name)
+	for _, pItem := range pythonProjectItems {
+		fmt.Println(pItem)
+	}
 	return nil
 }
