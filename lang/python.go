21d89ea933836d001a8fd3f5004ba9dfe0ae0ba3
carlosghabrous <carlos.ghabrous@gmail.com>
2021-11-23 17:11:42 +0100

    Add first iteration of python project items

--- lang/python.go
+++ lang/python.go
@@ -2,10 +2,51 @@ package lang
 
 import "fmt"
 
+const ReadmeContent = `SHORT DESCRIPTION OF PROJECT
+
+You can use [Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+## Purpose of this project
+## Getting started
+##%`
+
 type PythonProject Project
 
+//TODO: what to do with 'parent', 'permission'.
+//TODO: Need to distinguish between file and directory?
+
 var pythonProjectItems = []projectItem{
-	{name: "item1", parent: "parent1", permission: "permission1", content: "content1"},
+	{name: "README.md",
+		parent:     "",
+		permission: "permission1",
+		content:    ReadmeContent},
+	{name: "setup.py",
+		parent:     "",
+		permission: "",
+		content:    "setup.py content"},
+	{name: "",
+		parent:     "",
+		permission: "",
+		content:    ""},
+	{name: "__init__.py",
+		parent:     "",
+		permission: "",
+		content:    ""},
+	{name: "tests",
+		parent:     "",
+		permission: "",
+		content:    "",
+	},
+	{name: "__init__.py",
+		parent:     "tests",
+		permission: "",
+		content:    "",
+	},
+	{name: "test_",
+		parent:     "tests",
+		permission: "",
+		content:    ""},
 }
 
 func (p PythonProject) CheckNamingConventions() error {
