ae9be272fa64acfc182b60ff0d36958d607454ed
carlosghabrous <carlos.ghabrous@gmail.com>
2021-12-03 15:45:57 +0100

    Move types to registry, add tests for registry

--- registry/registry.go
+++ registry/registry.go
@@ -1,18 +1,51 @@
 package registry
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-type SupportedLanguages map[string]bool
+// projectItem represents an item in a project
+type projectItem struct {
+	name       string
+	parent     string //TODO: does go have a "directory"/file object
+	permission os.FileMode
+	content    string
+}
 
-var languageRegistry SupportedLanguages
+// Project represents a project in a certain language
+type Project struct {
+	Name         string
+	Language     string
+	projectItems []projectItem
+}
+
+// ProjectBuilder is an interface that builder objects implement within the lang package
+type ProjectBuilder interface {
+	CheckNamingConventions() error
+	Build() error
+}
+
+// Projects type stores builders for every language
+//TODO: replace ProjectBuilder by *Project
+type Projects map[string]*Project
+
+var languageRegistry Projects
 
 // RegisterLanguage allows modules in package lang to register themselves
-func RegisterLanguage(language string) {
+func RegisterLanguage(language string, project *Project) error {
 	if languageRegistry == nil {
-		languageRegistry = make(map[string]bool)
+		languageRegistry = make(map[string]*Project)
 	}
 
-	languageRegistry[language] = true
+	_, ok := languageRegistry[language]
+	if ok {
+		return fmt.Errorf("Language %s already registered. Are you sure you want to overwrite it?\n", language)
+	}
+
+	languageRegistry[language] = project
+
+	return nil
 }
 
 // LanguageSupported returns and error if argument language is not contained in the language registry
@@ -22,4 +55,10 @@ func LanguageSupported(language string) error {
 		return fmt.Errorf("Language %s is not supported\n", language)
 	}
 	return nil
+}
+
+func GetProject(language, name string) *Project {
+	project := languageRegistry[language]
+	project.Name = name
+	return project
 }
---
+++ registry/registry_test.go
@@ -0,0 +1,71 @@
+package registry
+
+import "testing"
+
+func cleanUpRegistry() func() {
+	return func() {
+		for k := range languageRegistry {
+			delete(languageRegistry, k)
+		}
+	}
+}
+
+func TestLanguageRegistryIsInitializedToNil(t *testing.T) {
+	if languageRegistry != nil {
+		t.Fatalf("Variable %s should not be allocated yet!\n", "languageRegistry")
+	}
+}
+
+func TestRegisterLanguage(t *testing.T) {
+	language, someLanguageProject := "some_language", Project{}
+	err := RegisterLanguage(language, &someLanguageProject)
+
+	cleanup := cleanUpRegistry()
+	defer cleanup()
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestAddSameLanguageTwiceFails(t *testing.T) {
+	language, someLanguageProject := "some_language", Project{}
+	err := RegisterLanguage(language, &someLanguageProject)
+	cleanup := cleanUpRegistry()
+	defer cleanup()
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	err = RegisterLanguage(language, &someLanguageProject)
+	if err == nil {
+		t.Fatalf(err.Error())
+	}
+
+}
+
+func TestLanguageSupported(t *testing.T) {
+	language, someLanguageProject := "some_language", Project{}
+	err := RegisterLanguage(language, &someLanguageProject)
+	cleanup := cleanUpRegistry()
+	defer cleanup()
+
+	err = LanguageSupported(language)
+	if err != nil {
+		t.Fatalf("Language %s has already been registered (%e)\n", language, err)
+	}
+}
+
+func TestGetProject(t *testing.T) {
+	language, someLanguageProject, projectName := "some_language", Project{}, "a_name"
+	RegisterLanguage(language, &someLanguageProject)
+	cleanup := cleanUpRegistry()
+	defer cleanup()
+
+	project := GetProject(language, projectName)
+
+	if project.Name != projectName {
+		t.Fatalf("Project name not correctly set!")
+	}
+}
