a071ade7b265154f7092c45d3a0e40c0f4e3fc4d
carlosghabrous <carlos.ghabrous@gmail.com>
2021-12-03 12:21:55 +0100

    Move functionality into skeleton package

--- lang/general.go
+++ lang/general.go
@@ -1,12 +1,8 @@
 package lang
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"path"
-	"strings"
 )
 
 const languageDirName = "lang"
@@ -33,54 +29,4 @@ type ProjectBuilder interface {
 	CheckNamingConventions() error
 	Build() error
 	SetAppName(appName string)
-}
-
-func init() {
-
-	initFilesToExclude()
-
-	files, err := ioutil.ReadDir(langDir)
-	if err != nil {
-		log.Fatal("Could not scan languages directory!")
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-
-		fileName := file.Name()
-
-		if _, ok := filesToExclude[fileName]; ok {
-			continue
-		}
-
-		if idx := strings.Index(fileName, ".go"); idx != -1 {
-			supportedLangs[fileName[:idx]] = true
-		}
-	}
-
-	initSupportedLanguages()
-}
-
-func initSupportedLanguages() {
-	supportedLangSlice = []string{}
-	for lan := range supportedLangs {
-		supportedLangSlice = append(supportedLangSlice, lan)
-	}
-}
-
-func initFilesToExclude() {
-	filesToExclude = map[string]bool{
-		"general.go": true,
-	}
-}
-
-func LanguageSupported(language string) error {
-	if _, ok := supportedLangs[language]; !ok {
-		errorMsg := fmt.Sprintf("Language %s not supported. Supported languages: %s", language, strings.Join(supportedLangSlice, ", "))
-		return fmt.Errorf(errorMsg)
-	}
-
-	return nil
 }
