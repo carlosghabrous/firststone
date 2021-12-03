e8a18fad5dca2bdddf44915e4bfd9125423d0e03
carlosghabrous <carlos.ghabrous@gmail.com>
2021-12-03 12:21:16 +0100

    Add skeleton package as replacement for lang.general

---
+++ skeleton/skeleton.go
@@ -0,0 +1,25 @@
+package skeleton
+
+import "fmt"
+
+type SupportedLanguages map[string]bool
+
+var languageRegistry SupportedLanguages
+
+// RegisterLanguage allows modules in package lang to register themselves
+func RegisterLanguage(language string) {
+	if languageRegistry == nil {
+		languageRegistry = make(map[string]bool)
+	}
+
+	languageRegistry[language] = true
+}
+
+// LanguageSupported returns and error if argument language is not contained in the language registry
+func LanguageSupported(language string) error {
+	_, ok := languageRegistry[language]
+	if !ok {
+		return fmt.Errorf("Language %s is not supported\n", language)
+	}
+	return nil
+}
