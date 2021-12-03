fae9d1abcd25d83c6396e960ff70601a67d3939a
carlosghabrous <carlos.ghabrous@gmail.com>
2021-12-03 21:48:29 +0100

    Add types to this file

---
+++ lang/types.go
@@ -0,0 +1,27 @@
+package lang
+
+import "os"
+
+// projectItem represents an item in a project
+type ProjectItem struct {
+	Name       string
+	Parent     string //TODO: does go have a "directory"/file object
+	Permission os.FileMode
+	Content    string
+}
+
+// Project represents a project in a certain language
+type Project struct {
+	Name         string
+	Language     string
+	ProjectItems []ProjectItem
+}
+
+// ProjectBuilder is an interface that builder objects implement within the lang package
+type ProjectBuilder interface {
+	CheckNamingConventions() error
+	Build() error
+}
+
+// Projects type stores builders for every language
+type Projects map[string]*Project
